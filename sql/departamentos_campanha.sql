5b49a8b9e47bcfcce79ce6fa425db43b180dde94
rodrigocunha093-stack <rodrigocunha093@gmail.com>
2026-06-16 15:43:10 -0300

    Atualizar departamentos para periodo mensal

--- sql/departamentos_campanha.sql
+++ sql/departamentos_campanha.sql
@@ -1,11 +1,11 @@
 -- BIGNORDESTE ANALYTICS
 -- Arquivo esperado: departamentos_campanha.txt
--- Objetivo: entender quais departamentos sustentam venda, lucro e margem da campanha.
--- Troque :id_loja, :mes, :data_inicio e :data_fim antes de executar.
+-- Troque :id_loja, :data_inicio e :data_fim.
+-- A coluna mes e gerada automaticamente por DATE_TRUNC.
 
 SELECT
     v.id_loja AS loja,
-    ':mes' AS mes,
+    TO_CHAR(DATE_TRUNC('month', v.data), 'YYYY-MM') AS mes,
     i.id_tipooferta,
     t.descricao AS campanha,
     p.mercadologico1,
@@ -14,20 +14,12 @@ SELECT
     SUM(i.quantidade) AS quantidade,
     SUM(i.valortotal) AS venda,
     SUM(i.valortotal) - SUM(i.customediocomimposto * i.quantidade) AS lucro,
-    ROUND(
-        (SUM(i.valortotal) - SUM(i.customediocomimposto * i.quantidade))
-        / NULLIF(SUM(i.valortotal),0) * 100
-    ,2) AS margem
+    ROUND((SUM(i.valortotal) - SUM(i.customediocomimposto * i.quantidade)) / NULLIF(SUM(i.valortotal),0) * 100,2) AS margem
 FROM pdv.venda v
-INNER JOIN pdv.vendaitem i
-    ON i.id_venda = v.id
-INNER JOIN public.tipooferta t
-    ON t.id = i.id_tipooferta
-INNER JOIN public.produto p
-    ON p.id = i.id_produto
-INNER JOIN public.mercadologico m
-    ON m.mercadologico1 = p.mercadologico1
-   AND m.nivel = 1
+INNER JOIN pdv.vendaitem i ON i.id_venda = v.id
+INNER JOIN public.tipooferta t ON t.id = i.id_tipooferta
+INNER JOIN public.produto p ON p.id = i.id_produto
+INNER JOIN public.mercadologico m ON m.mercadologico1 = p.mercadologico1 AND m.nivel = 1
 WHERE v.id_loja = :id_loja
   AND v.data >= ':data_inicio'
   AND v.data < ':data_fim'
@@ -35,10 +27,12 @@ WHERE v.id_loja = :id_loja
   AND i.oferta = TRUE
 GROUP BY
     v.id_loja,
+    DATE_TRUNC('month', v.data),
     i.id_tipooferta,
     t.descricao,
     p.mercadologico1,
     m.descricao
 ORDER BY
+    mes,
     campanha,
     venda DESC;
